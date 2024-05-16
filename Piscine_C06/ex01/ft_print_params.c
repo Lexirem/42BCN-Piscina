313734125674ae51d95ffc616282a4b7fcb98c54
Meritxell <70648240+Lexirem@users.noreply.github.com>
2024-05-16 10:53:49 +0200

    Add files via upload

---
+++ Piscine_C06/ex01/ft_print_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvallrib <mvallrib@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 10:47:09 by mvallrib          #+#    #+#             */
+/*   Updated: 2024/05/16 10:47:40 by mvallrib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (ac > 1)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			ft_putchar(av[i][j]);
+			j++;
+		}
+		i++;
+		ft_putchar('\n');
+	}
+	return (0);
+}
